851b27b57747d4e391be85f3789fd903178389f2
iprowriter <46689083+iprowriter@users.noreply.github.com>
2020-04-18 21:09:17 +0200

    updated with a slider

--- script.js
+++ script.js
@@ -6,3 +6,44 @@ $(document).ready(function() {
 })
 
 //the link above is jquery. Remember to add Jquery cdn to your index.html before using it
+
+//Below is the jquery link from https://kenwheeler.github.io/slick. This jquery powers the slideshow
+
+$('.post-wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next'),
+    prevArrow: $('.previous'),
+
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+  });
+              
